a8d7f2d14294507f09ebf83b51034f3e56877579
chairiandiputra <cakai.cp@gmail.com>
2018-03-05 11:42:26 +0700

    SplashScreen

---
+++ app/src/main/java/id/web/annisa/projectpop/SplashScreen.java
@@ -0,0 +1,30 @@
+package id.web.annisa.projectpop;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView tvSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L);
+    }
+}
