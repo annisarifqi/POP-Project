51730fe692e67a6cef6ce3b0f1cdf2956b67eba8
Farhan Dany <danyfarhan15@gmail.com>
2018-04-07 14:43:57 +0700

    1. Menghubungkan project ke firebase 2. Menambahkan layout biodata_pemesan

---
+++ app/src/main/java/id/web/annisa/projectpop/BiodataPemesan.java
@@ -0,0 +1,16 @@
+package id.web.annisa.projectpop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dany on 07/04/2018.
+ */
+
+public class BiodataPemesan extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.biodata_pemesan);
+    }
+}
+
---
+++ app/src/main/java/id/web/annisa/projectpop/Register.java
@@ -0,0 +1,15 @@
+package id.web.annisa.projectpop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dany on 20/03/2018.
+ */
+
+public class Register extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+    }
+}
