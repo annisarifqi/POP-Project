14fa59df808dce2e5d32ca33027b997175c66a26
milhamf15 <muhamadilhamfauzi15@gmail.com>
2018-04-09 13:50:01 +0700

    penambahan tab navigator

---
+++ app/src/main/java/id/web/annisa/projectpop/tab2_fragment.java
@@ -0,0 +1,32 @@
+package id.web.annisa.projectpop;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class tab2_fragment extends Fragment {
+    private static final String TAG = "tab2_fragment";
+    private Button BTNdaftarGedung;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab2_fragment,container,false);
+        BTNdaftarGedung = (Button) view.findViewById(R.id.buttonSimpanDaftarGedung);
+
+        BTNdaftarGedung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Anda berhasil mendaftar!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
