f3b995f05863dd4e9e7b436a5db02bf468e89a48
annisarifqi <icha.annisa98@gmail.com>
2018-04-09 12:49:44 +0700

    layout booking

--- app/src/main/java/id/web/annisa/projectpop/BookingActivity.java
+++ app/src/main/java/id/web/annisa/projectpop/BookingActivity.java
@@ -9,5 +9,6 @@ public class BookingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+        //layout booking
     }
 }
