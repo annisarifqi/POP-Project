416f43697a9d4a0f1c4aa918c396a8f76e0999f6
annisarifqi <icha.annisa98@gmail.com>
2018-04-23 09:33:28 +0700

    alert layout booking gedung

--- app/src/main/java/id/web/annisa/projectpop/BookingActivity.java
+++ app/src/main/java/id/web/annisa/projectpop/BookingActivity.java
@@ -1,13 +1,13 @@
 package id.web.annisa.projectpop;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+import android.widget.Toast;
 
 public class BookingActivity extends AppCompatActivity {
     private ImageView gambar;
@@ -28,7 +28,33 @@ public class BookingActivity extends AppCompatActivity {
         booking.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-               // Intent next = new Intent(BookingActivity.this, TransaksiActivity.class);
+                // Build the alert dialog.
+                AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(BookingActivity.this);
+                // Set the dialog title.
+                myAlertBuilder.setTitle(R.string.alert_title);
+                // Set the dialog message.
+                myAlertBuilder.setMessage(R.string.alert_message);
+                // Add the buttons.
+                myAlertBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button.
+                        Toast.makeText(getApplicationContext(), R.string.pressed_ok,
+                                Toast.LENGTH_SHORT).show();
+                        //Menuju ke class Transaksi
+                        // Intent next = new Intent(BookingActivity.this, TransaksiActivity.class);
+                    }
+                });
+                myAlertBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked the CANCEL button.
+                        Toast.makeText(getApplicationContext(), R.string.pressed_cancel,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // Create and show the AlertDialog.
+                myAlertBuilder.show();
+
             }
         });
 
